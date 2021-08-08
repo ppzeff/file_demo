b7cc57e01f510a5eb07e9e131c7ce51fb5d811b1
ppzeff <ppzeff@gmail.com>
2021-08-08 22:23:05 +0300

    first

---
+++ src/main/java/com/example/filedemo/controller/RedirectController.java
@@ -0,0 +1,28 @@
+package com.example.filedemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+    @GetMapping("/upload")
+    public String upload(Model model) {
+        model.addAttribute("title", "Сраница загрузки");
+        return "upload";
+    }
+
+    @GetMapping("/fire/{id}")
+    public String workersDetails(@PathVariable(value = "id") long id, Model model) {
+
+            return "redirect:https://danone-my.sharepoint.com/personal/aleksey_tolstik_danone_com/Documents/14001/test/test_v_1/bd/" + id + ".pdf";
+//        return "home";
+    }
+}
